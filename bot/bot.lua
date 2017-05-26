8ab8dd44ba5ddefea9f3ddfb7fc1fd92c18ede99
xmaxman <hsam4156@gmail.com>
2017-05-26 10:46:23 +0430

    Create bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -2,7 +2,7 @@ package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
     bot_token = "300285446:AAHXuACjlV5FZz4ncUaCZl0NTFpgONdF-94" --Put You Token Here
    send_api = "https://api.telegram.org/bot"..bot_token
-sudo_id = 157059515
+sudo_id = 
 require('./bot/methods')
 require('./bot/utils')
 require('./libs/JSON')
